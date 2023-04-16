56a71af77f34c6101aae6c4fd15c12b952f34f5f
Richard <rici.bernath@gmail.com>
2023-04-16 23:48:01 +0200

    Revert "Revert "2.4 - favicon, logos""
    This reverts commit f62fea021a60bbb444cad79aaa583360c7507cc9.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Article } from '../models/article';
 import { ArticleService } from '../services/article.service';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-
+import { Title } from "@angular/platform-browser"
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
@@ -15,7 +15,8 @@ export class ArticleComponent {
   content: SafeHtml;
   isLoaded: boolean = false;
 
-  constructor(private articleservice: ArticleService, private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
+  constructor(private articleservice: ArticleService, private route: ActivatedRoute, private sanitizer: DomSanitizer, private titleService: Title) {
+  }
 
   ngOnInit(): void {
     this.getArticleById(this.route.snapshot.params["id"]);
@@ -27,6 +28,7 @@ export class ArticleComponent {
         this.article = response;
         this.content = this.sanitizer.bypassSecurityTrustHtml(this.article.body.content);
         this.isLoaded = true;
+        this.titleService.setTitle(this.article.title);
       }
     );
   }
