71bd2c8618aeba1b827d46a5e9e62a7640646593
Richard <rici.bernath@gmail.com>
2023-12-07 22:19:36 +0100

    asd

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.0.2:8080',
+    apiUrl: 'http://localhost:8080',
     tinyMceApiKey: 'phtq55yffuuaqpv0c3tuyxkei4i2hvrjqjm7c3u5vo9fjvah'
 };
